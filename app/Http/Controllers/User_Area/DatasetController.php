2ea36184dd92885aec851412cb307a25df9aee8a
Goodarz Goodarznia <goodarznia@gmail.com>
2021-05-08 13:22:30 +0200

    * Complete Store method in DatasetController.php

--- app/Http/Controllers/User_Area/DatasetController.php
+++ app/Http/Controllers/User_Area/DatasetController.php
@@ -35,8 +35,33 @@ class DatasetController extends Controller
         $newFileName = now()->timestamp . '_' . $file->getClientOriginalName();
         $file->move(public_path('Uploaded_Files\\'), $newFileName);
         $file_path = public_path('Uploaded_Files\\') . $newFileName;
+        $datasets = json_decode(file_get_contents($file_path));
+        // Insert data in Data Explanation Table
+        $datasetExplanation_id = DatasetExplanation::insertGetId([
+            'title' => $Validate_data['Title'],
+            'activation_energy' => $Validate_data['Activation_Energy'],
+            'MKT' => calculation::calcMKT($Validate_data['Activation_Energy'], $datasets),
+            'comment' => $Validate_data['Comment'],
+            'user_id' => auth()->user()->id,
+            'user_ip' => request()->ip(),
+            'created_at' => now()->toDateTimeString(),
+            'updated_at' => now()->toDateTimeString()
+        ]);
 
+        // Insert data in Dataset Value Table
+        foreach ($datasets as $ds) {
 
+            DatasetValue::create([
+                'datasetExplanation_id' => $datasetExplanation_id,
+                'sample_time' => $ds->Time,
+                'sample_temperature' => $ds->Temperature
+            ]);
+
+        }
+        //Delete stored file for optimizing storage space
+        File::delete($file_path);
+
+        alert()->success('The dataset stored in Database properly', 'Congratulation')->persistent('OK');
         return redirect('/user_area/datasets');
     }
 }
